73e850918c18dd5f0ac9969cf8166c33250c0af6
Dev Chauhan <devgiri@iitk.ac.in>
2021-02-06 01:39:23 +0530

    Test for multiline comment and char

---
+++ tests/test3.c
@@ -0,0 +1,10 @@
+// comment test case
+int main(){
+    int a = 1 + 2;
+    int b; /* start of comment
+    one line
+        second line
+            c + d
+            */ char x = 'c';
+    return 0;
+}
