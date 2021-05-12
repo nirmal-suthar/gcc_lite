1aa2175a61c4fcc7187579d401576422aff00702
namanbiyani <namanb@iitk.ac.in>
2021-05-12 18:49:36 +0530

    math fn tc working

--- src/lib.py
+++ src/lib.py
@@ -5,17 +5,18 @@ int scanf(char *s);
 void strcpy(char *d, char *s);
 void* memcpy(void* d, void* s, int cnt);
 
-float fsin(float x);
-float fcos(float x);
-float ftan(float x);
-float fsininv(float x);
-float fcosinv(float x);
-float ftaninv(float x);
+float sinf(float x);
+float cosf(float x);
+float tanf(float x);
+float asinf(float x);
+float acosf(float x);
+float atanf(float x);
 
-float fexp(float x);
-float flog(float x);
-float fsqrt(float x);
-float fpow(float base, float power);
+float expf(float x);
+float logf(float x);
+float sqrtf(float x);
+float powf(float base, float power);
+
 
 // TODO: add malloc, free and math function
 
@@ -40,42 +41,42 @@ void * bufcpy(void * dest, void* src, int cnt){
 }
 
 float fsin(float x){
-    return sinf(x)
+    return sinf(x);
 }
 
 float fcos(float x){
-    return cosf(x)
+    return cosf(x);
 }
 
 float ftan(float x){
-    return tanf(x)
+    return tanf(x);
 }
 
 float fsininv(float x){
-    return asinf(x)
+    return asinf(x);
 }
 
 float fcosinv(float x){
-    return acosf(x)
+    return acosf(x);
 }
 
 float ftaninv(float x){
-    return atanf(x)
+    return atanf(x);
 }
 
 float flog(float x){
-    return logf(x)
+    return logf(x);
 }
 
 float fexp(float x){
-    return expf(x)
+    return expf(x);
 }
 
 float fsqrt(float x){
-    return sqrtf(x)
+    return sqrtf(x);
 }
 
 float fpow(float base, float power){
-    return powf(base, power)
+    return powf(base, power);
 }
 '''
