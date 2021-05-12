6bc5683572486141c4aa2a7acba833b946fb9b9a
namanbiyani <namanb@iitk.ac.in>
2021-05-12 12:01:52 +0530

    added math fns in lib

--- src/lib.py
+++ src/lib.py
@@ -4,6 +4,18 @@ int printf(char *s);
 int scanf(char *s);
 void strcpy(char *d, char *s);
 
+float fsin(float x);
+float fcos(float x);
+float ftan(float x);
+float fsininv(float x);
+float fcosinv(float x);
+float ftaninv(float x);
+
+float fexp(float x);
+float flog(float x);
+float fsqrt(float x);
+float fpow(float base, float power);
+
 // TODO: add malloc, free and math function
 
 // Extra functions
@@ -22,4 +34,43 @@ int scanint(){
     return tmp;
 }
 
+float fsin(float x){
+    return sinf(x)
+}
+
+float fcos(float x){
+    return cosf(x)
+}
+
+float ftan(float x){
+    return tanf(x)
+}
+
+float fsininv(float x){
+    return asinf(x)
+}
+
+float fcosinv(float x){
+    return acosf(x)
+}
+
+float ftaninv(float x){
+    return atanf(x)
+}
+
+float flog(float x){
+    return logf(x)
+}
+
+float fexp(float x){
+    return expf(x)
+}
+
+float fsqrt(float x){
+    return sqrtf(x)
+}
+
+float fpow(float base, float power){
+    return powf(base, power)
+}
 '''
