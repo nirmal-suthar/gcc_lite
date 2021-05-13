2997101371fe5db7c5f9d362946de0e1506cef3d
nirmal <nirmalps@iitk.ac.in>
2021-05-13 21:59:04 +0530

    Linked list working

--- tests/final/test20_linked_list.c
+++ tests/final/test20_linked_list.c
@@ -1,113 +1,55 @@
 int NULL;
+void * malloc(int size);
 
-struct node
-{
-    int data;          // Data
-    struct node *next; // Address
-} * head;
+struct node {
+   int data;
+   struct node *next;
+};
 
-/* Function declaration */
-void createList(int n);
-int  search(int key);
+struct node *head;
+struct node *current;
 
-int search(int key)
-{
-    int index;
-    struct node *curNode;
+//display the list
+void printList() {
 
-    index = 0;
-    curNode = head;
+   struct node *ptr = head;
 
-    // Iterate till last element until key is not found
-    while (curNode != NULL && curNode->data != key)
-    {
-        index++;
-        curNode = curNode->next;
-    }
+   printf("\n[head] =>");
+   //start from the beginning
+   while(ptr != NULL) {        
+      printf(" %d =>",ptr->data);
+      ptr = ptr->next;
+   }
 
-
-    return (curNode != NULL) ? index : -1;
+   printf(" [null]\n");
 }
 
-int main()
-{
-    int n, keyToSearch, index;
-    NULL=0;
-    // Input node count to create
-    printf("Enter number of node to create: ");
-    scanf("%d", &n);
+//insert link at the first location
+void insert(int data) {
+   //create a link
+   struct node *link = malloc(sizeof(struct node));
 
-    createList(n);
+   //link->key = key;
+   link->data = data;
 
-    // Input element to search from user.
-    printf("\nEnter element to search: ");
-    scanf("%d", &keyToSearch);
+   //point it to old first node
+   link->next = head;
 
-    // Call function to search first element by key
-    index = search(keyToSearch);
-    
-    // Element found in the list
-    if (index >= 0)
-        printf("%d found in the list at position %d\n", keyToSearch, index + 1);
-    else
-        printf("%d not found in the list.\n", keyToSearch);
-    
-
-    return 0;
+   //point first to new first node
+   head = link;
 }
 
-/**
- * Create a list of n nodes
- */
-void createList(int n)
-{
-    struct node *newNode, *temp;
-    int data, i;
+int main() {
+   head = 0;
+   current = 0;
+   
+   insert(10);
+   insert(20);
+   insert(30);
+   insert(1);
+   insert(40);
+   insert(56); 
 
-    head = malloc(sizeof(struct node));
-
-    /*
-     * Unable to allocate memory, hence exit from app.
-     */
-    if (head == NULL)
-    {
-        printf("Unable to allocate memory. Exiting from app.");
-        exit(0);
-    }
-    
-
-    /* Input head node data from user */
-    printf("Enter data of node 1: ");
-    scanf("%d", &data);
-
-    head->data = data; // Link data field with data
-    head->next = NULL; // Link address field to NULL
-
-    temp = head;
-
-    /*
-     * Create n nodes and add to the list
-     */
-    for (i = 2; i <= n; i++)
-    {
-        newNode = malloc(sizeof(struct node));
-
-        /* If memory is not allocated for newNode */
-        if (newNode == NULL)
-        {
-            printf("Unable to allocate memory. Exiting from app.");
-            exit(0);
-        }
-
-        printf("Enter data of node %d: ", i);
-        scanf("%d", &data);
-
-        newNode->data = data; // Link data field of newNode
-        newNode->next = NULL; // The newNode should point to nothing
-
-        temp->next = newNode; // Link previous node i.e. temp to the newNode
-        temp = temp->next;
-    }
-    
+   printList();
+   return 0;
 }
-
